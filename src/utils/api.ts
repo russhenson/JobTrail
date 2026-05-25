e357f0f01e3d76be88e6fdb6b54eaa47f6c2ed71
russhenson <henson.rj10@gmail.com>
2026-05-26 07:00:33 +0800

    fix: api endpoint

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,9 @@
 import axios from 'axios';
 import Config from 'react-native-config';
-import { Platform } from 'react-native';
 import { AuthStorage } from './authStorage';
 
-// Android emulator uses 10.0.2.2 to reach localhost
-const BASE_URL =
-    Platform.OS === 'android'
-        ? 'http://10.0.2.2:5050'
-        : Config.API_URL;
-
 export const api = axios.create({
-    baseURL: BASE_URL,
+    baseURL: Config.API_URL,
     headers: {
         'Content-Type': 'application/json',
     },
@@ -18,7 +11,7 @@ export const api = axios.create({
 
 // attach token to every request
 api.interceptors.request.use(
-    async (config) => {
+    async config => {
         const token = await AuthStorage.getToken();
 
         if (token && config.headers?.set) {
@@ -27,17 +20,17 @@ api.interceptors.request.use(
 
         return config;
     },
-    (error) => Promise.reject(error)
+    error => Promise.reject(error),
 );
 
 // auto logout
 api.interceptors.response.use(
-    (response) => response,
-    async (error) => {
+    response => response,
+    async error => {
         if (error.response?.status === 401) {
             await AuthStorage.clear();
         }
 
         return Promise.reject(error);
-    }
+    },
 );
